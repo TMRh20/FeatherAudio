6dd1df370891d390e745c5d19d6b252c03d06529
TMRh20 <tmrh20@gmail.com>
2026-02-28 06:48:56 -0600

    Update MP3 Player example

--- Examples/mp3Player/mp3Player.ino
+++ Examples/mp3Player/mp3Player.ino
@@ -15,7 +15,7 @@ AutoAnalog aaAudio;
 
 /************ USER CONFIGURATION ***************/
 const char* audioFilename = "noceil.mp3";
-uint8_t SD_CS_PIN = 2;                      // Set this to your CS pin for the SD card/module
+uint8_t SD_CS_PIN = 2;  // Set this to your CS pin for the SD card/module
 
 float volumeControl = 0.2;
 #define AUDIO_BUFFER_SIZE 1600  // Should be a multiple of 64
@@ -35,13 +35,13 @@ void setup() {
 
   aaAudio.I2S_PIN_LRCK = 28;  // Use different LRCK pin for Feather Express 52840
   aaAudio.maxBufferSize = AUDIO_BUFFER_SIZE;
-  aaAudio.begin(0,2);
+  aaAudio.begin(0, 2);
 
   playAudio(audioFilename);
 }
 
 void loop() {
-  
+
   loadBuffer();
 
   // Control via Serial for testing
@@ -54,12 +54,11 @@ void loop() {
       volumeControl = max(0.0, volumeControl);
     } else if (c == 'p') {
       playAudio("atliens.mp3");
-    } else if( c == 'c') {
+    } else if (c == 'c') {
       playAudio("dopeBoys.mp3");
     }
     Serial.println(volumeControl);
   }
-
 }
 
 
@@ -67,30 +66,32 @@ File myFile;
 
 void playAudio(const char* audioFile) {
 
+  MP3.end();
   if (myFile) {
-    MP3.end();
-    myFile.close();    
+    myFile.close();
   }
-
-  memset(aaAudio.dacBuffer16,0,sizeof(aaAudio.dacBuffer16));
-  aaAudio.feedDAC(0, 64);
+  aaAudio.disableDAC();
 
   //Open the designated file
   myFile = SD.open(audioFile);
 
-  MP3.begin(myFile);
-  MP3.getMetadata();
-  aaAudio.setSampleRate(MP3.Fs, 1);
-  Serial.println(MP3.Fs);
-  aaAudio.dacBitsPerSample = MP3.bitsPerSample;
-  Serial.println(MP3.bitsPerSample);
+  if (myFile) {
+    MP3.begin(myFile);
+    MP3.getMetadata();
+    aaAudio.setSampleRate(MP3.Fs, 1);
+    Serial.println(MP3.Fs);
+    aaAudio.dacBitsPerSample = MP3.bitsPerSample;
+    Serial.println(MP3.bitsPerSample);
+  }else{
+    Serial.println("Failed to open file");
+  }
 }
 
 void loadBuffer() {
 
   uint32_t sampleCounter = 0;
   if (myFile.available() >= AUDIO_BUFFER_SIZE) {
-    for (uint32_t i = 0; i < AUDIO_BUFFER_SIZE/64; i++) {
+    for (uint32_t i = 0; i < AUDIO_BUFFER_SIZE / 64; i++) {
       audio = MP3.decode();
       memcpy(&aaAudio.dacBuffer16[sampleCounter], audio.interleaved, 128);  // 128 bytes
       sampleCounter += 64;                                                  // 64 samples per go
@@ -102,6 +103,6 @@ void loadBuffer() {
     }
     aaAudio.feedDAC(0, sampleCounter);
   } else {
-    myFile.seek(0);
+    aaAudio.disableDAC();
   }
 }
